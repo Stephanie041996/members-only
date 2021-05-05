a9a8489c60b04bf455f85ed5c7bba7968be38f20
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-05-05 10:01:11 +0100

    Set up devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'members_only/Post'
+  root 'members_only#Post'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
