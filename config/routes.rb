c8915758cef98d94660aff11fccca4ab0e98cc14
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-05-05 10:16:05 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'members_only/Post'
   root 'members_only#Post'
+  get 'members_only/Post'
+  resources :post, only [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
